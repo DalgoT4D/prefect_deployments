cc7c5a774119a6b97fbb52e0c34823ad925dc57b
Think201 <think201@Think201---Mac-101.local>
2022-12-20 11:31:46 +0530

    deployment file and taking connection id from env

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
-from prefect import flow
+from prefect import flow, task
 from prefect_airbyte.connections import trigger_sync
 from prefect_dbt.cli.commands import trigger_dbt_cli_command
+from dotenv import load_dotenv
 
+load_dotenv()
 
-@flow(name="Run Airbyte Sync")
+@flow(name="airbyte-sync")
 def run_airbyte_sync():
       trigger_sync(
-            connection_id="7a2c0dc6-c08b-4bc3-8cab-760e64ae8a8f",
+            connection_id=os.getenv('STIR_SURVEY_CTO_AIRBYTE'),
             poll_interval_s=3,
             status_updates=True
       )
       return 1
 
-@flow(name="Run dbt Transform")
+@flow(name="dbt-transform")
 def run_dbt_transform():
       trigger_dbt_cli_command(
             command="dbt run", project_dir='stir'
       )
       return 1
 
-@flow(name="Orchestration flow")
+@flow(name="orchestration-flow")
 def run_flow():
 
       #syncing airbyte
