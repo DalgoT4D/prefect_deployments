4559921c8a3c2647609876f31f6d47441529ce1e
Think201 <think201@Think201---Mac-101.local>
2023-01-19 17:40:37 +0530

    dost prefect setup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from prefect import flow, task
 from prefect_airbyte.connections import trigger_sync
 from prefect_dbt.cli.commands import trigger_dbt_cli_command
+from prefect_shell import shell_run_command
 from dotenv import load_dotenv
 import os
 
@@ -9,16 +10,24 @@ load_dotenv()
 @flow(name="airbyte-sync")
 def run_airbyte_sync():
       trigger_sync(
-            connection_id=os.getenv('STIR_SURVEY_CTO_AIRBYTE'),
+            connection_id=os.getenv('DOST_AIRBYTE_CONNECTION'),
             poll_interval_s=3,
             status_updates=True
       )
       return 1
 
+@flow(name="github-pull")
+def pull_dost_github_repo():
+      shell_run_command('rm -rf dbt && git clone '+ os.getenv('DOST_GITHUB_URL'))
+      return 1
+
 @flow(name="dbt-transform")
 def run_dbt_transform():
       trigger_dbt_cli_command(
-            command="dbt run", project_dir='stir'
+            command="dbt deps", project_dir='dbt'
+      )
+      trigger_dbt_cli_command(
+            command="dbt run", project_dir='dbt'
       )
       return 1
 
@@ -28,9 +37,26 @@ def run_flow():
       #syncing airbyte
       run_airbyte_sync()
 
+      # pull dbt repo
+      pull_dost_github_repo()
+
       #dbt transform
       run_dbt_transform()
 
+@flow(name="orchestrate-airbyte")
+def run_airbyte_flow():
+
+      #syncing airbyte
+      run_airbyte_sync()
+
+@flow(name="orchestrate-dbt")
+def run_dbt_flow():
+
+      # pull dbt repo
+      pull_dost_github_repo()
+
+      #dbt transform
+      run_dbt_transform()
 
 if __name__ == "__main__":
-      run_flow()
+      run_dbt_flow()
